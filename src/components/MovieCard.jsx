0ff3a126889fbd0f885da2681525e0559ef35a33
subh-23 <subhodiproy281@gmail.com>
2022-11-05 09:35:49 +0530

    only remain work is making the search button work

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -2,35 +2,40 @@ import React from "react";
 
 export const MovieCard = ({ movie }) => {
   return (
-    <div class="max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+    <div className="max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
       <a href="#">
-        <img class="rounded-t-lg" src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg" alt="" />
+        <img className="rounded-t-lg" width="100%" height="100%" src={movie.Poster} alt={movie.Title} />
       </a>
-      <div class="p-5">
+      <div className="p-5">
         <a href="#">
-          <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-            {movie.title}
+          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+            {movie.Title}
           </h5>
         </a>
-        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
-          {movie.actors}
+        <div className="flex flex-row justify-start space-x-2">
+        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+          {movie.Year}
         </p>
+        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+          {movie.Type}
+        </p>
+        </div>
         <a
           href="#"
-          class="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          className="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         >
           Read more
           <svg
             aria-hidden="true"
-            class="ml-2 -mr-1 w-4 h-4"
+            className="ml-2 -mr-1 w-4 h-4"
             fill="currentColor"
             viewBox="0 0 20 20"
             xmlns="http://www.w3.org/2000/svg"
           >
             <path
-              fill-rule="evenodd"
+              fillRule="evenodd"
               d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-              clip-rule="evenodd"
+              clipRule="evenodd"
             ></path>
           </svg>
         </a>
--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -8,10 +8,20 @@ export const Movies = ({ allMovies }) => {
       <div className="text-center m-20 text-2xl text-white font-medium">
         All {Input} Movies
       </div>
-      <div class="grid gap-6 md:grid-cols-3 m-20">
-        {allMovies.map((movie) => (
+      <div className="grid gap-6 md:grid-cols-3 m-20">
+        {allMovies.length > 0 ? (
+          allMovies.map((movie) => (
+            <MovieCard movie={movie} key={movie.imdbID} />
+          ))
+        ) : (
+          <div className="text-center text-2xl text-white font-medium">
+            No Movies Found
+          </div>
+        )}
+        {/* {allMovies.map((movie) => (
           <MovieCard movie={movie} />
-        ))}
+        ))} */}
+        {/* <MovieCard movie={allMovies}/> */}
       </div>
     </>
   );
